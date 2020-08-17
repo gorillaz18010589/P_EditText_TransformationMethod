1f4eaa2f0af5bb8529bf45eda27cf332ba2e9dcd
jennifer <gorillaz1801058@gmail.com>
2020-08-17 20:46:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/p_edittext_transformationmethod/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.p_edittext_transformationmethod;
+//EditTexy 顯示密碼/隱藏功能
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et_password;
+    private ImageView ivEye;
+    private boolean isOpenEye = false;
+    //绑定id
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        et_password = findViewById(R.id.et_password);
+        ivEye = findViewById(R.id.iv_eye);
+
+        //密码是否可见
+        ivEye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isOpenEye) {
+                    ivEye.setSelected(true);
+                    isOpenEye = true;
+                    //密码可见
+                    et_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else{
+                    ivEye.setSelected(false);
+                    isOpenEye = false;
+                    //密码不可见
+                    et_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+    }
+}
